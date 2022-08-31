5661aea6cb2e2a0a1f737111b81505e0bea6ccb5
nicolaas <nweidema@usc.edu>
2022-08-30 22:00:24 -0700

    Fix bug in call function pair property calculator

--- bcd/call_function_pair_property_calculator.py
+++ bcd/call_function_pair_property_calculator.py
@@ -4,14 +4,16 @@ from bcd.function_pair_property_calculator import FunctionPairPropertyCalculator
 class CallFunctionPairPropertyCalulator(FunctionPairPropertyCalculator):
 
     def _get_property(self, i, j):
-        func1 = self._func_list[i]
-        func2 = self._func_list[j]
+        func1_addr = self._func_list[i]
+        func2_addr = self._func_list[j]
+
+        func1 = self._cfg.functions.function(addr=func1_addr)
 
         num_calls_to_func2 = 0
         for call_site_addr in func1.get_call_sites():
             call_target = func1.get_call_target(call_site_addr)
             assert call_target is not None
-            if call_target == func2.addr:
+            if call_target == func2_addr:
                 num_calls_to_func2 += 1
 
         return num_calls_to_func2
