adba15042a217fa7dd521c66f56947ba6c5611ff
nicolaas <nweidema@usc.edu>
2022-09-10 08:38:32 -0700

    Update tests to use new API

--- tests/test_data_reference_function_pair_property_calculator.py
+++ tests/test_data_reference_function_pair_property_calculator.py
@@ -1,8 +1,10 @@
 from nose.tools import *
 import os
 import angr
+from elftools.elf.elffile import ELFFile
 
 from bcd.data_ref_function_pair_property_calculator import DataRefFunctionPairPropertyCalulator
+from bcd.sections import Section
 
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'binaries', 'test_binaries', 'bin')
@@ -10,6 +12,7 @@ test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 
 def test001():
     elf_file_path = os.path.join(test_location, 'test001')
 
+    elffile = ELFFile(open(elf_file_path, 'rb'))
     proj = angr.Project(elf_file_path, auto_load_libs=False)
     cfg = proj.analyses.CFGFast(normalize=True)
 
@@ -17,7 +20,9 @@ def test001():
 
     func_name_to_index = {cfg.functions.function(addr=func_addr).name: i for i, func_addr in enumerate(func_list)}
 
-    drfpp = DataRefFunctionPairPropertyCalulator(proj, cfg, func_list)
+    sections = elffile.iter_sections()
+    section_offsets = [Section(sec).compute_section_offsets() for sec in sections]
+    drfpp = DataRefFunctionPairPropertyCalulator(proj, cfg, func_list, section_offsets)
     
     p = _func_name_get_property('f1', 'f2', func_name_to_index, drfpp)
     assert_equal(p, 0)
@@ -26,7 +31,7 @@ def test001():
     assert_equal(p, 0)
 
     p = _func_name_get_property('f5', 'f4', func_name_to_index, drfpp)
-    assert_equal(p, 0)
+    assert_equal(p, 1)
 
     p = _func_name_get_property('f4', 'f6', func_name_to_index, drfpp)
     assert_equal(p, 1)
@@ -38,7 +43,7 @@ def test001():
     assert_equal(p, 2)
 
     p = _func_name_get_property('f7', 'f2', func_name_to_index, drfpp)
-    assert_equal(p, 2)
+    assert_equal(p, 0)
 
     p = _func_name_get_property('f4', 'f7', func_name_to_index, drfpp)
     assert_equal(p, 0)
@@ -53,7 +58,8 @@ def test001():
 def _func_name_get_property(func1_name, func2_name, func_name_to_index, drfpp):
     i = func_name_to_index[func1_name]
     j = func_name_to_index[func2_name]
-    p = drfpp.get_property(i, j)
+    print(drfpp.get_property(i, j))
+    p = len(drfpp.get_property(i, j))
     return p
 
 
